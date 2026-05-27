dfd3459d6c7f00349a59c154a4a78ed3e1ace588
Анастасия <nastyonaa@MacBook-Air-Anastasia.local>
2026-05-27 14:27:16 +0300

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "io.github.nastyoonaa"
-version = "1.0.6"
+version = "1.0.7"
 
 repositories {
     mavenCentral()
@@ -44,7 +44,7 @@ mavenPublishing {
     coordinates(
         "io.github.nastyoonaa",
         "idiomatic-architecture-viewer",
-        "1.0.6"
+        "1.0.7"
     )
 
     pom {
